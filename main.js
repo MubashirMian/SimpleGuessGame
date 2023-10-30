79092090c427dc4f74846c2e59cb9bb0bb1ce776
MubashirMian <138796628+MubashirMian@users.noreply.github.com>
2023-10-30 16:27:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from 'inquirer';
+import { Toinquire, getRandomNumber } from './inquiry.js';
+const levels = [
+    'Easy',
+    'Medium',
+    'Hard'
+];
+const selectLevelPrompt = [
+    {
+        type: 'list',
+        name: 'LvlOfDiff',
+        message: 'Select the level difficulty you prefer\n',
+        choices: levels
+    }
+];
+inquirer.prompt(selectLevelPrompt).then(answers => {
+    const criteria = answers.LvlOfDiff;
+    console.log('Selected Level:', criteria);
+    if (criteria == levels[0]) {
+        const ToGuess = getRandomNumber(1, 10);
+        console.log(ToGuess);
+        Toinquire("1-10", ToGuess);
+    }
+    else if (criteria == levels[1]) {
+        const ToGuess = getRandomNumber(1, 100);
+        console.log(ToGuess);
+        Toinquire("1-100", ToGuess);
+    }
+    else {
+        const ToGuess = getRandomNumber(1, 1000);
+        console.log(ToGuess);
+        Toinquire("1-1000", ToGuess);
+    }
+});
